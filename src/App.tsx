b67c789fb337a02669f3150e1a030b4e6ea304a0
〽️iraya <mirayaabrodi@gmail.com>
2024-02-16 09:20:34 +0100

    change: uninstall react-router-dom and firebase and install zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Routes, Route } from "react-router-dom";
 import { Home } from "./components";
 
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </div>
   );
 }
